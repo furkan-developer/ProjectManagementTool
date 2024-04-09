54c9a1b1777bff2ba44bbf48d2a9d1413158e271
furkan-developer <furknn2112@gmail.com>
2024-04-09 15:14:58 +0300

    ADDED AppRoles helper

---
+++ ProjectManagement.WebApp/Helpers/Identity/AppRoles.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManagement.WebApp.Helpers.Identity
+{
+    public static class AppRoles
+    {
+        public const string Admin = "admin";
+        public const string ProjectManager = "project-manager";
+        public const string User = "user";
+        public const string ProjectUser= "project-user";
+    }
+}
