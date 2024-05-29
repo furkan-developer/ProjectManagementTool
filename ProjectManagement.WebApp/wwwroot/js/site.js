ee4e52b5cebdb440d9bc7edbfbd88500abc834f8
furkan aydin <furknn2112@gmail.com>
2024-05-29 04:02:44 +0300

    UPDATED dynamically task creating

--- ProjectManagement.WebApp/wwwroot/js/site.js
+++ ProjectManagement.WebApp/wwwroot/js/site.js
@@ -218,41 +218,6 @@ function createNewTask({ title, id, dueDate, priority, assignments }) {
     assignmentList.appendChild(assignmentItem1);
   }
 
-  // Plus icon SVG
-  const plusIconSvg = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "svg"
-  );
-  plusIconSvg.setAttribute("stroke", "currentColor");
-  plusIconSvg.setAttribute("fill", "currentColor");
-  plusIconSvg.setAttribute("stroke-width", "0");
-  plusIconSvg.setAttribute("viewBox", "0 0 512 512");
-  plusIconSvg.setAttribute("height", "1em");
-  plusIconSvg.setAttribute("width", "1em");
-
-  // Paths for plus icon SVG
-  const plusIconPath1 = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "path"
-  );
-  plusIconPath1.setAttribute(
-    "d",
-    "M346.5 240H272v-74.5c0-8.8-7.2-16-16-16s-16 7.2-16 16V240h-74.5c-8.8 0-16 6-16 16s7.5 16 16 16H240v74.5c0 9.5 7 16 16 16s16-7.2 16-16V272h74.5c8.8 0 16-7.2 16-16s-7.2-16-16-16z"
-  );
-  plusIconSvg.appendChild(plusIconPath1);
-
-  const plusIconPath2 = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "path"
-  );
-  plusIconPath2.setAttribute(
-    "d",
-    "M256 76c48.1 0 93.3 18.7 127.3 52.7S436 207.9 436 256s-18.7 93.3-52.7 127.3S304.1 436 256 436c-48.1 0-93.3-18.7-127.3-52.7S76 304.1 76 256s18.7-93.3 52.7-127.3S207.9 76 256 76m0-28C141.1 48 48 141.1 48 256s93.1 208 208 208 208-93.1 208-208S370.9 48 256 48z"
-  );
-  plusIconSvg.appendChild(plusIconPath2);
-
-  taskFooterWrapperDiv.appendChild(plusIconSvg);
-
   return taskWrapper;
 }
 
